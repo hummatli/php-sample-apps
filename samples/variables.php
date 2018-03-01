74dc7e1eb96fb39cd792ee44ec655ad81c38be47
Sattar Hummatli <settarxan@gmail.com>
2018-03-01 15:57:10 +0400

    try variables and strings

---
+++ samples/variables.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Variables</title>
+</head>
+<body>
+    <?php 
+
+    $var1 = 5;
+    echo $var1;
+
+    echo "<br />";
+
+    $var1 = 15;
+    echo $var1;     
+    
+    echo "<br />";
+
+    $var2 = "Hello World";
+    echo $var2;
+
+    ?>
+</body>
+</html>
